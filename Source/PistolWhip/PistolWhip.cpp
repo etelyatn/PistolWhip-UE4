7dab7c447771dc261b04ded6c78b2ebb190fcdce
EugeneTel <eugene.telyatnik@gmail.com>
2021-01-02 13:08:48 +0100

    Initial commit

---
+++ Source/PistolWhip/PistolWhip.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "PistolWhip.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, PistolWhip, "PistolWhip" );
